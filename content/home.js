9bb1af9026d0e2070979c69d009b19ab7066860c
Drew Botka <drewbotka@gmail.com>
2017-10-02 15:10:36 -0400

    added contact form to home page

--- content/home.js
+++ content/home.js
@@ -21,6 +21,14 @@ const homeContent = {
     {
       name: "Jimmy",
       bio: "Capitalize on low hanging fruit to identify a ballpark value added activity to beta test."
+    },
+    {
+      name: "Timmy",
+      bio: "At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution."
+    },
+    {
+      name: "Jimmy",
+      bio: "Capitalize on low hanging fruit to identify a ballpark value added activity to beta test."
     }
   ]
 }
